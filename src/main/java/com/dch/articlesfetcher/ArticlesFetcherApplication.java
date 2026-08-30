6016859ac162b46525bc5e0488079db2a78c82c2
DCH <dariuz.chmiela@gmail.com>
2026-08-30 07:30:44 +0200

    Initial commit

---
+++ src/main/java/com/dch/articlesfetcher/ArticlesFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.dch.articlesfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesFetcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dch/articlesfetcher/ArticlesFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dch.articlesfetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesFetcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
