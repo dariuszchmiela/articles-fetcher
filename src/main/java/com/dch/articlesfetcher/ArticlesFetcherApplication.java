a62ae5fab81ccab379bb5b1ac98836bf2ccc25ef
DCH <dariuz.chmiela@gmail.com>
2026-08-30 08:22:12 +0200

    Enable configuration properties scanning

--- src/main/java/com/dch/articlesfetcher/ArticlesFetcherApplication.java
+++ src/main/java/com/dch/articlesfetcher/ArticlesFetcherApplication.java
@@ -2,12 +2,12 @@ package com.dch.articlesfetcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ArticlesFetcherApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ArticlesFetcherApplication.class, args);
     }
-
 }
