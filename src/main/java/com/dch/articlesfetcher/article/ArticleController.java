09a734bfe9f47616684c33afd1fbf97cefc73004
DCH <dariuz.chmiela@gmail.com>
2026-08-30 09:37:48 +0200

    Add mark-as-read endpoint

--- src/main/java/com/dch/articlesfetcher/article/ArticleController.java
+++ src/main/java/com/dch/articlesfetcher/article/ArticleController.java
@@ -1,7 +1,11 @@
 package com.dch.articlesfetcher.article;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,5 +27,11 @@ public class ArticleController {
         return articleService.getUnreadArticles().stream()
                 .map(articleDtoMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping("/{id}/read")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void markAsRead(@PathVariable Long id) {
+        articleService.markAsRead(id);
     }
 }
