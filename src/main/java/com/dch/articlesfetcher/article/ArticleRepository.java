f8de6efabf70fd25c5985c0e8ee96b82bd34a085
DCH <dariuz.chmiela@gmail.com>
2026-08-30 09:25:35 +0200

    Add unread articles lookup with limit

--- src/main/java/com/dch/articlesfetcher/article/ArticleRepository.java
+++ src/main/java/com/dch/articlesfetcher/article/ArticleRepository.java
@@ -1,13 +1,17 @@
 package com.dch.articlesfetcher.article;
 
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Set;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     @Query("select a.externalId from Article a where a.externalId in :externalIds")
     Set<Long> findExistingExternalIds(@Param("externalIds") Set<Long> externalIds);
+
+    List<Article> findByReadAtIsNullOrderByExternalIdAsc(Limit limit);
 }
